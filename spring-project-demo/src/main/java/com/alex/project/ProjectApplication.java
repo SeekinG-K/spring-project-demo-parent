c8dc692f813effcf45e38fa983904742d67b03f9
SeekinG-K <937310638@qq.com>
2020-12-06 17:21:33 +0800

    adjust mybatis config

--- spring-project-demo/src/main/java/com/alex/project/ProjectApplication.java
+++ spring-project-demo/src/main/java/com/alex/project/ProjectApplication.java
@@ -1,11 +1,9 @@
 package com.alex.project;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.alex.project.dao")
 public class ProjectApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProjectApplication.class, args);
--- spring-project-demo/src/main/java/com/alex/project/config/MybatisConfig.java
+++ spring-project-demo/src/main/java/com/alex/project/config/MybatisConfig.java
@@ -1,10 +1,12 @@
 package com.alex.project.config;
 
 import com.baomidou.mybatisplus.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@MapperScan("com.alex.project.dao")
 public class MybatisConfig {
 
     /**
